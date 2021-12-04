f2860fa9fb4ce4880f6f41d1294ec1e99c45f741
Straivers <straivers98@gmail.com>
2021-12-03 23:21:11 -0500

    Cast first rays

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,46 @@ mod image;
 mod math;
 mod ray;
 
+use math::{float3, Point};
+use ray::Ray;
+use color::Rgb;
+
+fn ray_color(ray: &Ray) -> Rgb {
+    let dir = ray.direction.normalized();
+    let t = 0.5 * (dir.y() + 1.0);
+    let c = (1.0 - t) * float3::new(1.0, 1.0, 1.0) + t * float3::new(0.5, 0.7, 1.0);
+    Rgb::from_f32(c.x(), c.y(), c.z())
+}
+
 fn main() {
-    let width = 256;
-    let height = 256;
+    // Image
+    let aspect = 16.0 / 9.0 as f32;
+    let width = 400;
+    let height = (width as f32 / aspect) as u32;
+
+    // Camera
+    let viewport_height = 2.0 as f32;
+    let viewport_width = aspect * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point::new(0.0, 0.0, 0.0);
+    let horizontal = float3::new(viewport_width, 0.0, 0.0);
+    let vertical = float3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - float3::new(0.0, 0.0, focal_length);
+
     let mut pixels = Vec::new();
 
     for j in 0..height {
         for i in 0..width {
-            pixels.extend_from_slice(&color::Rgb::from_f32(
-                i as f32 / (width as f32 - 1.0),
-                j as f32 / (height as f32 - 1.0),
-                0.25,
-            ).as_u8());
+            let u = i as f32 / (width as f32 - 1.0);
+            let v = j as f32 / (height as f32 - 1.0);
+
+            let r = Ray{
+                origin,
+                direction: lower_left_corner.as_vec() + u * horizontal + v * vertical - origin.as_vec(),
+            };
+
+            pixels.extend_from_slice(&ray_color(&r).as_u8());
         }
     }
 
--- src/math.rs
+++ src/math.rs
@@ -1,10 +1,14 @@
-use std::ops::{Add, Div, Mul, Neg, Sub};
+use std::ops::{Add, Div, Mul, Neg, Sub, DivAssign};
 
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct float3([f32; 3]);
 
 impl float3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self([x, y, z])
+    }
+
     pub fn x(&self) -> f32 {
         self.0[0]
     }
@@ -23,6 +27,33 @@ impl float3 {
 
     pub fn length_squared(&self) -> f32 {
         self.0[0] * self.0[0] + self.0[1] * self.0[1] + self.0[2] * self.0[2]
+    }
+
+    pub fn normalize(&mut self) {
+        *self /= self.length();
+    }
+
+    /// Returns a normalized version of this vector.
+    pub fn normalized(&self) -> Self {
+        let mut other = *self;
+        other.normalize();
+        other
+    }
+
+    pub fn dot(&self, rhs: Self) -> Self {
+        Self([
+            self.0[0] * rhs.0[0],
+            self.0[1] * rhs.0[1],
+            self.0[2] * rhs.0[2],
+        ])
+    }
+
+    pub fn cross(&self, rhs: Self) -> Self {
+        Self([
+            self.0[1] * rhs.0[2] - self.0[2] * rhs.0[1],
+            self.0[2] * rhs.0[0] - self.0[0] * rhs.0[2],
+            self.0[0] * rhs.0[1] - self.0[1] * rhs.0[0]
+        ])
     }
 }
 
@@ -50,6 +81,14 @@ impl Add<f32> for float3 {
 
     fn add(self, rhs: f32) -> Self::Output {
         Self([self.0[0] + rhs, self.0[1] + rhs, self.0[2] + rhs])
+    }
+}
+
+impl Add<float3> for f32 {
+    type Output = float3;
+
+    fn add(self, rhs: float3) -> Self::Output {
+        rhs + self
     }
 }
 
@@ -89,6 +128,13 @@ impl Mul<f32> for float3 {
     }
 }
 
+impl Mul<float3> for f32 {
+    type Output = float3;
+    fn mul(self, rhs: float3) -> Self::Output {
+        rhs * self
+    }
+}
+
 impl Div for float3 {
     type Output = Self;
     fn div(self, rhs: Self) -> Self {
@@ -107,10 +153,22 @@ impl Div<f32> for float3 {
     }
 }
 
+impl DivAssign<f32> for float3 {
+    fn div_assign(&mut self, rhs: f32) {
+        self.0[0] /= rhs;
+        self.0[1] /= rhs;
+        self.0[2] /= rhs;
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Point([f32; 3]);
 
 impl Point {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self([x, y, z])
+    }
+
     pub fn x(&self) -> f32 {
         self.0[0]
     }
@@ -121,6 +179,10 @@ impl Point {
 
     pub fn z(&self) -> f32 {
         self.0[2]
+    }
+
+    pub fn as_vec(&self) -> float3 {
+        float3(self.0)
     }
 }
 
